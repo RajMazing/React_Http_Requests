653e6ddc9f240ef6479b6c08182d425a981202ee
RajMazing <caseyraj92@gmail.com>
2022-07-13 02:15:10 -0500

    using firebase to post request data. had to change api link from star wars to firebase link given

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
 
+
+import AddMovie from "./components/AddMovie";
+
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -17,7 +20,8 @@ function App() {
 		setError(null);
 
 		try {
-			const response = await fetch("https://swapi.dev/api/films/");
+			// const response = await fetch("https://swapi.dev/api/films/");
+			const response = await fetch("https://react-http-requests-78a78-default-rtdb.firebaseio.com/movies.json");
 			if (!response.ok) {
 				throw new Error("SOMETHING WENT WRONG!");
 			}
@@ -44,6 +48,10 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
 	let content = <p>Found no movies!</p>;
 
 	if (movies.length > 0) {
@@ -58,6 +66,9 @@ function App() {
 
 	return (
 		<React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
