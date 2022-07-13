fff284283f3553955bf935912f094708d1903005
RajMazing <caseyraj92@gmail.com>
2022-07-13 15:54:11 -0500

    Using firebase and fetch api to send POST requests

--- src/App.js
+++ src/App.js
@@ -28,16 +28,19 @@ function App() {
 
 			const data = await response.json();
 
+			const loadedMovies = [];
+
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+
+				})
+			}
 			//setting the parameters to be displayed or fetched
-			const transformedMovies = data.results.map((movieData) => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMovies);
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -48,8 +51,17 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+   const response = await fetch('https://react-http-requests-78a78-default-rtdb.firebaseio.com/movies.json', {
+		method: 'POST',
+		body: JSON.stringify(movie),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	 })
+	 const data = await response.json();
+	 console.log(data)
+
   }
 
 	let content = <p>Found no movies!</p>;
